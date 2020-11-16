f9e7c33193371ff7deca453f01262ee7f4478e85
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-16 15:13:33 +0900

    Add exercise to workouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_051210) do
+ActiveRecord::Schema.define(version: 2020_11_16_055935) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.integer "sets"
+    t.integer "reps"
+    t.integer "workout_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["workout_id"], name: "index_exercises_on_workout_id"
+  end
 
   create_table "workouts", force: :cascade do |t|
     t.datetime "date"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_11_16_051210) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "exercises", "workouts"
 end
